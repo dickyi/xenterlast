dea686c645bbe7ad3bf087a11f671a5f5ff9768a
dickyi <irawandicky300@gmail.com>
2024-09-27 18:34:08 +0700

    bunga11

---
+++ src/xenter/Main.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package xenter;
+
+
+
+/**
+ *
+ * @author Irawa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bunga_Kamboja bunga = new Bunga_Kamboja();
+
+        bunga.setnama_ilmiah("Plumeria rubra");
+        bunga.setWarna("Merah");
+        bunga.setIklim("Tropis");
+        bunga.setTanah("Berdrainase baik");
+        bunga.setHewan_penyerbuk("Lebah");
+
+        System.out.println("\t \t \t Bunga Kamboja");
+        System.out.println("Nama Ilmiah: " + bunga.getnama_ilmiah());
+        System.out.println("Warna: " + bunga.getWarna());
+        System.out.println("Iklim: " + bunga.getIklim());
+        System.out.println("Tanah: " + bunga.getTanah());
+        System.out.println("Hewan Penyerbuk: " + bunga.getHewan_penyerbuk());
+        
+        
+        
+
+    }
+}
